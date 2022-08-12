4a53775cf80cc792502622c3589e5ab5ae7174a3
Pierre-Luc Bertrand <bertrand.pierreluc@gmail.com>
2022-08-11 22:54:08 -0400

    temp

--- docker/lotus-miner/start.sh
+++ docker/lotus-miner/start.sh
@@ -31,4 +31,8 @@ mkdir /snapshot
 cd /snapshot
 curl -sI https://fil-chain-snapshots-fallback.s3.amazonaws.com/mainnet/minimal_finality_stateroots_latest.car | perl -ne '/x-amz-website-redirect-location:\s(.+)\.car/ && print "$1.sha256sum\n$1.car"' | xargs wget
 
+ls /snapshot | sed 's|\(.*\)\.\(.*\)|mv -f /snapshot/\1.\2 /snapshot/latest.\2|' | bash
+
+
+
 echo "Done!"
