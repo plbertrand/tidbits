a347a65ba9a7aa6205b1d537a9f3fbe8319a3172
Pierre-Luc Bertrand <bertrand.pierreluc@gmail.com>
2022-09-22 22:18:14 -0400

    Fix bugs

--- docker/timemachine/install.sh
+++ docker/timemachine/install.sh
@@ -3,6 +3,7 @@ systemctl --user stop timemachine
 mkdir -p $HOME/.config/systemd/user
 cp timemachine.service $HOME/.config/systemd/user/timemachine.service
 systemctl --user daemon-reload
+systemctl --user enable timemachine
 ./run.sh
 systemctl --user restart timemachine
 
