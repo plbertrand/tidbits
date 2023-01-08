732d02cb605dd09c5361034120b5c5b4abef3488
Pierre-Luc Bertrand <bertrand.pierreluc@gmail.com>
2023-01-08 00:31:13 -0500

    Update to v1.20 and fixes

--- docker/lotus-build/build-intel.sh
+++ docker/lotus-build/build-intel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-buildah bud --pull --layers -t plbertrand/lotus-build-intel:latest -f Dockerfile.intel --build-arg BRANCH="v1.17.1" .
+buildah bud --pull --layers -t plbertrand/lotus-build-intel:latest -f Dockerfile.intel --build-arg BRANCH="release/v1.20.0" .
