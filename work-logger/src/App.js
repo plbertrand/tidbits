c65bdf5fd64daeaedb0e84e3abcf8b3f102e2701
Pierre-Luc Bertrand <bertrand.pierreluc@gmail.com>
2023-12-29 21:44:28 -0800

    Fetch message from server

--- work-logger/src/App.js
+++ work-logger/src/App.js
@@ -1,23 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [msg, setMsg] = useState();
+  useEffect(async () => {
+    try {
+      const res = await axios.get("/welcome");
+      console.log(res);
+      setMsg(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  console.log(msg);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {msg}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
