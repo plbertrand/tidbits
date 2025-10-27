35d2fabeb66be1096ec04291730293cc6b702680
Pierre-Luc Bertrand <bertrand.pierreluc@gmail.com>
2025-10-27 09:30:44 -0700

    Add sudoers file

--- datadog/install.sh
+++ datadog/install.sh
@@ -3,14 +3,17 @@
 # Define source files (relative to script location)
 LOCAL_CHECK_FILE="checks.d/temperatures.py"
 LOCAL_CONF_FILE="conf.d/temperatures.yaml"
+LOCAL_SUDOERS_FILE="sudoers.d/dd-temperatures-smartctl"
 
 # Define destination paths on the local machine
 DEST_CHECK_DIR="/etc/datadog-agent/checks.d/"
 DEST_CONF_DIR="/etc/datadog-agent/conf.d/"
+DEST_SUDOERS_DIR="/etc/sudoers.d/"
 
 # Ensure destination directories exist
 sudo mkdir -p "${DEST_CHECK_DIR}"
 sudo mkdir -p "${DEST_CONF_DIR}"
+sudo mkdir -p "${DEST_SUDOERS_DIR}"
 
 # Copy check file
 echo "Copying ${LOCAL_CHECK_FILE} to ${DEST_CHECK_DIR}"
@@ -19,6 +22,10 @@ sudo cp "${LOCAL_CHECK_FILE}" "${DEST_CHECK_DIR}"
 # Copy configuration file
 echo "Copying ${LOCAL_CONF_FILE} to ${DEST_CONF_DIR}"
 sudo cp "${LOCAL_CONF_FILE}" "${DEST_CONF_DIR}"
+
+# Copy sudoers file
+echo "Copying ${LOCAL_SUDOERS_FILE} to ${DEST_SUDOERS_DIR}"
+sudo cp "${LOCAL_SUDOERS_FILE}" "${DEST_SUDOERS_DIR}"
 
 # Restart Datadog agent
 echo "Restarting Datadog agent"
